3eb0c1d1407331e451cde4696df1f23aa3ae7500
wojtas000 <jan2000@op.pl>
2023-03-15 20:06:41 +0100

    Update files

---
+++ weighted_random_search.py
@@ -0,0 +1,91 @@
+import random
+
+def wrs_step(F, params:dict, F_value:float, p:dict, k:dict, iteration:int, param_grid:dict):
+    thresh = random.uniform(0,1)
+    new_params = {}
+    for hypar in params:
+        if p[hypar]>=thresh or iteration < k[hypar]:
+            new_params[hypar] = random.choice(param_grid[hypar])
+        else:
+            new_params[hypar] = params[hypar]
+    if new_params == params:
+        return params, F_value
+    
+    F_new_value = F(new_params)
+
+    if F_new_value > F_value:
+        return new_params, F_new_value  
+    else:
+        return params, F_value
+    
+def fanova(param_history, F_value_history, param_grid):
+    p = {key: 1 for key in param_grid}
+    k = {key: 3 for key in param_grid}
+    return p, k
+
+def wrs(F, N:int, N_0:int, param_grid:dict):
+
+    starting_params = {key: random.choice(value) for key, value in param_grid.items()}
+    param_history = [0 for i in range(N_0)]
+    F_value_history = [0 for i in range(N_0)]
+    p = {key: 1 for key in param_grid}
+    k = {key: 3 for key in param_grid}
+    params = starting_params
+    F_value = F(params)
+    print(params, F_value)
+    
+    for iter in range(N_0):
+        params, F_value = wrs_step(F=F, params=params, F_value=F_value, p=p, k=k, 
+                                   iteration=iter, param_grid=param_grid )
+        param_history[iter] = params
+        F_value_history[iter] = F_value
+        print(params, F_value)
+    
+    p, k = fanova(param_history, F_value_history, param_grid)
+
+
+    for iter in range(N - N_0):
+        params, F_value = wrs_step(F=F, params=params, F_value=F_value, p=p, k=k, 
+                                   iteration=iter, param_grid=param_grid )
+        print(params, F_value)
+    
+    return params, F_value    
+
+
+if __name__ == '__main__':
+    
+    def add(params):
+        sum = 0
+        for param in params:
+            sum += params[param]
+        return sum
+
+    params = {'lr': 0.01, 'batch_size': 32}
+    param_grid = {'lr': [0.01, 0.05, 0.1], 'batch_size': [32, 64, 128]}
+    p = {'lr': 0.5, 'batch_size': 0.5}
+    k = {'lr': 3, 'batch_size': 5}
+    iteration = 4
+    F = add
+    F_value = F(params)
+    N, N_0 = 10, 3
+    print(wrs_step(F, params, F_value, p, k, iteration, param_grid))
+    print(wrs(F, N, N_0, param_grid))
+
+# import pandas as pd
+# import numpy as np
+# import ConfigSpace 
+# from ConfigSpace.hyperparameters import UniformFloatHyperparameter
+# from fanova import fANOVA
+
+# pima = pd.read_csv('pima.csv', header=0)
+# features = pima.iloc[:, :-1]
+# responses = pima.iloc[:, -1]
+# pcs = list(zip(np.min(features,axis=0), np.max(features, axis=0)))
+# print(pcs)
+# cs = ConfigSpace.ConfigurationSpace()
+# for i in range(len(pcs)):
+# 	cs.add_hyperparameter(UniformFloatHyperparameter("%i" %i, pcs[i][0], pcs[i][1]))
+# cs
+
+# dims = (1, )
+# f = fANOVA(X = features, y=responses, config_space=cs)
