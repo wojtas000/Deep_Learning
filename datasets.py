3c66b6cb8ab434d227be73e62f83c5de0ca8faa9
wojtas000 <jan2000@op.pl>
2023-03-17 15:51:23 +0100

    Updates

--- datasets.py
+++ datasets.py
@@ -114,71 +114,6 @@ class CifarDataset(Dataset):
         fig.subplots_adjust(hspace=0.35)
         plt.show()
 
-
-class MixedDataset(Dataset):
-    def __init__(self, dataset1, dataset2):
-        self.dataset1 = dataset1
-        self.dataset2 = dataset2
-        self.permutation = list(range(len(self.dataset1)))
-        random.shuffle(self.permutation)
-    
-    def __len__(self):
-        return len(self.dataset1)
-
-    def __getitem__(self, index):
-        # get images from the two Datasets
-        image1, label = self.dataset1[index]
-        image2, _ = self.dataset2[self.permutation[index]]
-
-        # mix the two images using tensor_mixer
-        mixed_image = MixedDataset.tensor_mixer(image1, image2)
-
-        # return the mixed image and a dummy label
-        return mixed_image, label
-    
-    @staticmethod
-    def tensor_mixer(tensor1, tensor2):
-        return torch.mean(torch.stack([tensor1, tensor2]), dim=0)
-
-    def display_sample_images(self, sample=None, sample_size=4, resize=128):
-        """
-        Function for displaying sample of images from dataset. The sample can be random or chosen by the user.
-        Params:
-        sample - list of indices of the images in Dataset
-        sample_size - number of randomly selected images, if sample == None
-        resize - value used for resizing the image. For example if resize=128, we get 128x128 pixels image.
-        """
-        if sample:
-            random_sample = sample
-        else:
-            random_sample = random.sample(range(len(self)), sample_size)
-        fig, axes = plt.subplots(nrows = int(np.ceil(sample_size/2)) , ncols = 2, figsize=(6, sample_size))
-        row, col = 0, 0
-        
-    
-        for i in random_sample:
-            img, _ = self[i]
-            label = self.dataset1.labels.iloc[i, 1]
-            resized_img = interpolate(img.unsqueeze(0), size=(resize, resize), mode='bilinear', align_corners=False).squeeze(0)
-
-            if sample_size <=2:
-                axes[col].imshow(resized_img.permute(1,2,0))
-                axes[col].set_title(label)
-            else:
-                axes[row, col].imshow(resized_img.permute(1,2,0))
-                axes[row, col].set_title(label)
-            
-            if col == 0:
-                col += 1
-            else:
-                row += 1
-                col = 0
-        
-        fig.suptitle('Random sample')
-        fig.subplots_adjust(hspace=0.35)
-        plt.show()
-
-
 class GaussianNoise(object):
     """
     Class used for adding Gaussian noise to image. 
@@ -213,18 +148,28 @@ class Cutout(object):
 
         return img
 
+class MixUpTransform(object):
+    def __init__(self, alpha=0.5, dataset = None):
+        self.alpha = alpha
+        self.dataset = dataset
 
-# DATASETS, DATALOADERS, TRANSFORMERS
+    def __call__(self, img):
+        mix_img, _ = self.dataset[int(torch.randperm(len(self.dataset))[0])]
 
-transformer = transforms.Compose([transforms.ToTensor(), 
+        mixed = img * self.alpha + mix_img * (1 - self.alpha) 
+
+        return mixed
+
+# ORIGINAL TRAIN AND VALIDATION DATASETS
+
+default_transformer = transforms.Compose([transforms.ToTensor(), 
                                   transforms.Normalize((0.5,), (0.5,))])
 
-
-# Create pyTorch Dataset instances of training and validation data
 cifar_train = CifarDataset(root_dir = TRAIN_DIR, labels=TRAIN_LABELS, 
-                           transform=transformer, class_dict=CLASS_DICT)
+                           transform=default_transformer, class_dict=CLASS_DICT)
 cifar_val = CifarDataset(root_dir = VAL_DIR, labels=VAL_LABELS, 
-                         transform=transformer, class_dict=CLASS_DICT)
+                         transform=default_transformer, class_dict=CLASS_DICT)
+
 
 # Simple augmentation technique
 transformer1 = transforms.Compose([transforms.Pad(padding=(4, 4, 4, 4), fill=0, padding_mode='constant'),
@@ -234,44 +179,41 @@ transformer1 = transforms.Compose([transforms.Pad(padding=(4, 4, 4, 4), fill=0, 
                                   transforms.ToTensor(),
                                   transforms.Normalize((0.5,), (0.5,)),
                                   GaussianNoise(mean=0, std=0.01)])
-                                    
 
-
-cifar_aug1 = CifarDataset(root_dir = TRAIN_DIR, labels=TRAIN_LABELS, 
-                          transform=transformer1, class_dict=CLASS_DICT)
-
-# Merge training data with augmented data
-merged_dataset1 = ConcatDataset([cifar_train, cifar_aug1])
 
 # Advanced augmentation technique - mixing
 transformer2 = transforms.Compose([transforms.Pad(padding=(4, 4, 4, 4), fill=0, padding_mode='constant'),
                                   transforms.RandomCrop(IMG_SIZE),
                                   transforms.RandomHorizontalFlip(p=0.5),
                                   transforms.ToTensor(),
-                                  transforms.Normalize((0.5,), (0.5,))])
+                                  transforms.Normalize((0.5,), (0.5,)),
+                                  MixUpTransform(alpha=0.6, dataset=cifar_train)])
                                   
-                                    
-cifar_aug2 = CifarDataset(root_dir = TRAIN_DIR, labels=TRAIN_LABELS, 
-                                      transform=transformer2, class_dict=CLASS_DICT), 
-
-mixed_dataset = MixedDataset(cifar_aug2, cifar_aug2)
-
-# Merge training data with mixed data
-merged_dataset2 = ConcatDataset([cifar_train, mixed_dataset])
 
 # Advanced augmentation technique - cutout
 transformer3 = transforms.Compose([Cutout(size=10, p=1),
                                   transforms.ToTensor(), 
                                   transforms.Normalize((0.5,), (0.5,))])
+                                  
+                                    
+# DATASETS, DATALOADERS
 
-cifar_aug4 = CifarDataset(root_dir = TRAIN_DIR, labels=TRAIN_LABELS, 
-                          transform=transformer3, class_dict=CLASS_DICT)
+# Original
+cifar_train = CifarDataset(root_dir = TRAIN_DIR, labels=TRAIN_LABELS, 
+                           transform=default_transformer, class_dict=CLASS_DICT)
+cifar_val = CifarDataset(root_dir = VAL_DIR, labels=VAL_LABELS, 
+                         transform=default_transformer, class_dict=CLASS_DICT)
 
-merged_dataset3 = ConcatDataset([cifar_train, cifar_aug4])
-
-# Create DataLoader instances
 train_loader = DataLoader(cifar_train, batch_size=32, shuffle=True)
 val_loader = DataLoader(cifar_val, batch_size=32, shuffle=False)
-augmentation1_loader = DataLoader(merged_dataset1, batch_size=32, shuffle=True)
-augmentation2_loader = DataLoader(merged_dataset2, batch_size=32, shuffle=True)
-augmentation3_loader = DataLoader(merged_dataset3, batch_size=32, shuffle=True)
+
+#augmented
+cifar_basic_aug = CifarDataset(root_dir = TRAIN_DIR, labels=TRAIN_LABELS, 
+                          transform=transformer1, class_dict=CLASS_DICT)
+cifar_mixup = CifarDataset(root_dir = TRAIN_DIR, labels=TRAIN_LABELS, 
+                                      transform=transformer2, class_dict=CLASS_DICT)
+cifar_cutout = CifarDataset(root_dir = TRAIN_DIR, labels=TRAIN_LABELS, 
+                          transform=transformer3, class_dict=CLASS_DICT)
+basic_aug_loader = DataLoader(cifar_basic_aug, batch_size=32, shuffle=True)
+mixup_loader = DataLoader(cifar_mixup, batch_size=32, shuffle=True)
+cutout_loader = DataLoader(cifar_cutout, batch_size=32, shuffle=True)
